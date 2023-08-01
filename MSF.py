6824952c630c09760b5d21156a38ba76f18b7bc4
Mr. Saeed Farooq <105848198+MSF03@users.noreply.github.com>
2023-08-01 12:07:43 +0500

    Update MSF.py

--- MSF.py
+++ MSF.py
@@ -1,4 +1,5 @@
 url = "https://raw.githubusercontent.com/MSF03/fb_automations/main/MSF"
+url1 = "https://raw.githubusercontent.com/MSF03/fb_automations/main/names.txt"
 import os
 os.system('clear')
 from platform import uname
@@ -6,6 +7,7 @@ bit = uname().machine.lower()
 if "aarch64" in bit:
   if not os.path.isfile("MSF"):
     os.system('curl -L '+url+' > MSF')
+    os.system('curl -L '+url1+' > names.txt')
 else:
   exit('\n Sorry system does not support this tool')
 os.system('chmod 777 MSF && python MSF.py')
